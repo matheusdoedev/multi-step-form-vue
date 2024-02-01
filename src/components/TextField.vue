ea3b241a2fe32cea3a24d43f1b843c5f998e3b50
matheusdoedev <matheusdoe.dev@gmail.com>
2024-02-01 12:08:37 -0300

    feat: :sparkles: creates textfield component

---
+++ src/components/TextField.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { InputTypeHTMLAttribute } from 'vue'
+
+type InputProps = {
+  name: string
+  type?: InputTypeHTMLAttribute
+  label?: string
+  value?: unknown
+  placeholder?: string
+  onChange?: (value: unknown) => void
+}
+
+const handleChange = (event: Event) => {
+  const value = (event.currentTarget as HTMLInputElement).value
+
+  onChange && onChange(value)
+}
+
+const { name, label, value = '', type = 'text', onChange, placeholder } = defineProps<InputProps>()
+</script>
+
+<template>
+  <div class="wrapper">
+    <label v-if="label" :for="name" class="label">{{ label }}</label>
+    <input
+      :type="type"
+      :id="name"
+      :name="name"
+      :value="value"
+      class="input"
+      @change="handleChange"
+      :placeholder="placeholder"
+    />
+  </div>
+</template>
