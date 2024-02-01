83bc8a63f0a850f8487f40f2d2e3cf810a189b63
matheusdoedev <matheusdoe.dev@gmail.com>
2024-02-01 12:07:22 -0300

    feat: :sparkles: creates home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import PersonalInfoStep from '@/components/PersonalInfoStep.vue'
+
+const FORM_STEPS = [
+  { order: 1, name: 'info', title: 'Your Info' },
+  { order: 2, name: 'plan', title: 'Select Plan' },
+  { order: 3, name: 'add-ons', title: 'Add-Ons' },
+  { order: 4, name: 'summary', title: 'Summary' }
+]
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <aside>
+        <ul>
+          <li v-for="step in FORM_STEPS" :key="step.order">
+            <div>{{ step.order }}</div>
+            <div>
+              <span>Step {{ step.order }}</span>
+              <h3>{{ step.title }}</h3>
+            </div>
+          </li>
+        </ul>
+      </aside>
+      <section>
+        <PersonalInfoStep />
+        <footer>
+          <button>Go Back</button>
+          <button>Next Step</button>
+        </footer>
+      </section>
+    </div>
+  </main>
+</template>
